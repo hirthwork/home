424c41214d61b806fc1b7b87d47ca654d274d64d
Dmitry Potapov <dpotapov@yandex-team.ru>
2017-01-13 21:37:20 +0300

    .emacs.d/jabber-config.el: show message notifications through the xosd

--- .emacs.d/jabber-config.el
+++ .emacs.d/jabber-config.el
@@ -35,3 +35,24 @@
 ; Add function last in chain, so message will be stored in jabber history
 (add-to-list 'jabber-message-chain 'jabber-handle-incoming-xep-0184-request t)
 
+(defun jabber-message-xosd (from buffer text proposed-alert)
+  "Display incoming message event through the xosd"
+  (let*
+    ((process-connection-type nil)
+    (process-name (start-process
+      "jabber-xosd"
+      nil
+      "osd_cat"
+      "-i800"
+      "-f-misc-fixed-bold-r-normal--15-140-75-75-c-90-*-*"
+      "-l100"
+      "-o30"
+      "-s1"
+      "-clightgreen"
+      "-")))
+    (process-send-string process-name text)
+    (process-send-eof process-name)))
+
+(require 'jabber-alert)
+(add-to-list 'jabber-alert-message-hooks 'jabber-message-xosd)
+
