87cd318b78e9eb3730b9dac69bcb7ec86b5688b0
Dmitry Potapov <potapov.d@gmail.com>
2017-01-13 20:35:40 +0300

    .emacs.d/jabber-config.el: XEP-0184 support

--- .emacs.d/jabber-config.el
+++ .emacs.d/jabber-config.el
@@ -12,3 +12,25 @@
 
 (add-hook 'jabber-chat-mode-hook 'flyspell-mode)
 (add-hook 'jabber-chat-mode-hook 'point-im-mode)
+
+(require 'jabber-core)
+(require 'jabber-xml)
+
+(defun jabber-handle-incoming-xep-0184-request (jc xml-data)
+  "Check for XEP-0184 request and send delivery confirmation if any"
+  (let
+    ((id (jabber-xml-get-attribute xml-data 'id))
+    (xmlns
+      (jabber-xml-get-attribute
+        (car (jabber-xml-get-children xml-data 'request))
+        'xmlns)))
+    (if (string= xmlns "urn:xmpp:receipts")
+      (jabber-send-sexp
+        jc
+        `(message
+           ((to . ,(jabber-xml-get-attribute xml-data 'from)))
+           (received ((xmlns . "urn:xmpp:receipts") (id . ,id))))))))
+
+; Add function last in chain, so message will be stored in jabber history
+(add-to-list 'jabber-message-chain 'jabber-handle-incoming-xep-0184-request t)
+
