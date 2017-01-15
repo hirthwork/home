ff026e76a93795d26bd6844e2e4f6c0cbacab23e
Dmitry Potapov <dpotapov@yandex-team.ru>
2017-01-15 22:07:58 +0300

    .emacs.d/jabber-status.el: do not call dwm update if it is not needed

--- .emacs.d/jabber-status.el
+++ .emacs.d/jabber-status.el
@@ -1,15 +1,22 @@
+(defun dwm-status-update ()
+  (start-process-shell-command "dwm-update" nil "~/.dwm.update"))
+
 (defun jabber-status-update ()
   "Updates dwm status with list of contacts with unread messages"
   (if
     jabber-activity-jids
-    (with-temp-file "~/.emacs.d/jabber-status"
-      (insert
-        (mapconcat
-          (lambda (jid) (concat "⎁" (cdr jid)))
-          (mapcar 'jabber-activity-lookup-name jabber-activity-jids)
-          " ")))
-    (ignore-errors (delete-file "~/.emacs.d/jabber-status")))
-  (start-process-shell-command "dwm-update" nil "~/.dwm.update"))
+    (progn
+      (with-temp-file "~/.emacs.d/jabber-status"
+        (insert
+          (mapconcat
+            (lambda (jid) (concat "⎁" (cdr jid)))
+            (mapcar 'jabber-activity-lookup-name jabber-activity-jids)
+            " ")))
+      (dwm-status-update))
+    (ignore-errors
+      (progn
+        (delete-file "~/.emacs.d/jabber-status")
+        (dwm-status-update)))))
 
 (require 'jabber-activity)
 (add-hook 'jabber-activity-update-hook 'jabber-status-update)
