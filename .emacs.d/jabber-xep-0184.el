d1a4890589eb250a294e09e3ecab2af8319931a8
Dmitry Potapov <dpotapov@yandex-team.ru>
2017-01-15 22:36:01 +0300

    .emacs.d/jabber-xep-0184.el: fix infinite delivery receipt sending, add delivery receipt in service discovery features

--- .emacs.d/jabber-xep-0184.el
+++ .emacs.d/jabber-xep-0184.el
@@ -6,17 +6,20 @@
   (let
     ((id (jabber-xml-get-attribute xml-data 'id)))
     (when
-      (string=
-        (jabber-xml-get-attribute
-          (car (jabber-xml-get-children xml-data 'request))
-          'xmlns)
-        "urn:xmpp:receipts"))
+      (and
+        id
+        (string=
+          (jabber-xml-get-attribute
+            (car (jabber-xml-get-children xml-data 'request))
+            'xmlns)
+          "urn:xmpp:receipts"))
       (jabber-send-sexp
         jc
         `(message
            ((to . ,(jabber-xml-get-attribute xml-data 'from)))
-           (received ((xmlns . "urn:xmpp:receipts") (id . ,id)))))))
+           (received ((xmlns . "urn:xmpp:receipts") (id . ,id))))))))
 
 ;; Add function last in chain, so message will be stored in jabber history
 (add-to-list 'jabber-message-chain 'jabber-handle-incoming-xep-0184-request t)
+(add-to-list 'jabber-advertised-features "urn:xmpp:receipts")
 
