bdb75c070a9ff0ee71e207d6496cfa3fbba37ed5
Dmitry Potapov <dpotapov@yandex-team.ru>
2017-06-07 13:53:09 +0300

    .emacs.d/spell-config.el: disable de_DE dictionary as it non compatible with unmasked hunspell

--- .emacs.d/spell-config.el
+++ .emacs.d/spell-config.el
@@ -1,3 +1,3 @@
 (setq ispell-program-name "hunspell")
 (setq ispell-local-dictionary "en_US")
-(setq ispell-local-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US,ru_RU_yo,de_DE") nil utf-8)))
+(setq ispell-local-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US,ru_RU_yo") nil utf-8)))
