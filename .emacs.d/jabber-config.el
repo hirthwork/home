a2aad19e5fbe43fbefd67ac9b8ec783b2860fab1
Dmitry Potapov <dpotapov@yandex-team.ru>
2018-02-15 16:58:41 +0300

    .emacs.d/jabber-config.el: detailed date format

--- .emacs.d/jabber-config.el
+++ .emacs.d/jabber-config.el
@@ -7,6 +7,8 @@
 (setq jabber-roster-show-bindings nil)
 (setq jabber-backlog-number 20)
 (setq jabber-backlog-days 7)
+(setq jabber-chat-time-format "%F %T")
+(setq jabber-chat-delayed-time-format "%F %T (offline)")
 
 (add-to-list 'load-path "~/.emacs.d/emacs-point-el")
 (require 'point-im)
